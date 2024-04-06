556e7d4ee28fabc08ab41677551577cc732f376b
fujimori <fujimori_mateo@hotmail.com>
2024-04-06 18:20:23 -0300

    Selenium error handling

--- auto_play.py
+++ auto_play.py
@@ -3,15 +3,26 @@ import time
 import pyautogui
 import pyscreeze
 import PILTools
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import login
 
 def button_for_play(driver, button_html):
-        button_play = driver.find_element(By.XPATH, button_html)
-        button_play.click()
-        time.sleep(45)
-        driver.refresh()
-        time.sleep(7)
+        max_retries = 3
+        attempt = 0
+        while attempt < max_retries:
+                try:
+                        button_play = driver.find_element(By.XPATH, button_html)
+                        button_play.click()
+                        time.sleep(45)
+                        driver.refresh()
+                        time.sleep(7)
+                        break
+                except NoSuchElementException:
+                        attempt += 1
+
+
+
 
 
 
